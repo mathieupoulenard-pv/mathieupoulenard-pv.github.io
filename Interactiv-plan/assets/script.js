933b9e6f2695f62f5640968eb1216c5ac989c45f
andreaVoivoi <63197866+andreaVoivoi@users.noreply.github.com>
2021-06-03 15:53:48 +0200

    Update script.js

--- Interactiv-plan/assets/script.js
+++ Interactiv-plan/assets/script.js
@@ -1,26 +1,82 @@
 jQuery(document).ready( function () {
 
+    // Zoom mobile
+    var eventsHandler;
 
-    window.zoomFabrics = svgPanZoom('#demoSVG', {
-        zoomEnabled: true,
-        controlIconsEnabled: true,
-        fit: true,
-        center: true,
-        // viewportSelector: document.getElementById('demo-tiger').querySelector('#g4') // this option will make library to misbehave. Viewport should have no transform attribute
-      });
+    eventsHandler = {
+        haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel']
+        , init: function(options) {
+            var instance = options.instance
+            , initialScale = 1
+            , pannedX = 0
+            , pannedY = 0
 
-      document.getElementById('enable').addEventListener('click', function() {
-        window.zoomFabrics.enableControlIcons();
-      })
-      document.getElementById('disable').addEventListener('click', function() {
-        window.zoomFabrics.disableControlIcons();
-      })
+            // Init Hammer
+            // Listen only for pointer and touch events
+            this.hammer = Hammer(options.svgElement, {
+            inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchInput
+            })
 
+            // Enable pinch
+            this.hammer.get('pinch').set({enable: true})
+
+            // Handle double tap
+            this.hammer.on('doubletap', function(ev){
+            instance.zoomIn()
+            })
+
+            // Handle pan
+            this.hammer.on('panstart panmove', function(ev){
+            // On pan start reset panned variables
+            if (ev.type === 'panstart') {
+                pannedX = 0
+                pannedY = 0
+            }
+
+            // Pan only the difference
+            instance.panBy({x: ev.deltaX - pannedX, y: ev.deltaY - pannedY})
+            pannedX = ev.deltaX
+            pannedY = ev.deltaY
+            })
+
+            // Handle pinch
+            this.hammer.on('pinchstart pinchmove', function(ev){
+            // On pinch start remember initial zoom
+            if (ev.type === 'pinchstart') {
+                initialScale = instance.getZoom()
+                instance.zoomAtPoint(initialScale * ev.scale, {x: ev.center.x, y: ev.center.y})
+            }
+
+            instance.zoomAtPoint(initialScale * ev.scale, {x: ev.center.x, y: ev.center.y})
+            })
+
+            // Prevent moving the page on some devices when panning over SVG
+            options.svgElement.addEventListener('touchmove', function(e){ e.preventDefault(); });
+        }
+
+        , destroy: function(){
+            this.hammer.destroy()
+        }
+    }
+
+    jQuery("#searchSupplier").val('');
+    // Show maps
     jQuery(".show-fabrics").on('click touch', function() {
         jQuery("#map-min").hide();
         jQuery("#map-details").show();
         jQuery("#back").show();
+        window.panZoom = svgPanZoom('#demoSVG', {
+            zoomEnabled: true,
+            controlIconsEnabled: true,
+            zoomScaleSensitivity: 0.5,
+            maxZoom: 15,
+            fit: 1,
+            center: 1,
+            customEventsHandler: eventsHandler
+        });
     });
+
+    // Back button
     jQuery("#back").on('click touch', function() {
         jQuery("#searchSupplier").val('');
         jQuery("#live-search").hide();
@@ -29,6 +85,7 @@ jQuery(document).ready( function () {
         jQuery("#back").hide();
     });
 
+    // Search
     // Search
     var qs = jQuery('input#searchSupplier').quicksearch('#demoSVG text', {
         'delay': 100,
@@ -66,14 +123,4 @@ jQuery(document).ready( function () {
             jQuery("#no-result").show();
         },
     });
-
-    jQuery(document).on('click', '.go-supplier', function() {
-        console.log('click')
-        var zoomInX = jQuery(this).data('x');
-        var zoomInY = jQuery(this).data('y');
-        console.log(zoomInX)
-        console.log(zoomInY)
-    });
 });
-
-// Zoom
